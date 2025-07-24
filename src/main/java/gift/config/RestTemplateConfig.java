baf15ee742d9ec4a68c203bc3f7fe901609b0e61
Junyoung Yang <y@jun0.dev>
2025-07-25 03:33:35 +0900

    feat: RestTemplate Timeout 설정(5초)

---
+++ src/main/java/gift/config/RestTemplateConfig.java
@@ -0,0 +1,19 @@
+package gift.config;
+
+import java.time.Duration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder
+                .connectTimeout(Duration.ofSeconds(5))
+                .readTimeout(Duration.ofSeconds(5))
+                .build();
+    }
+}
