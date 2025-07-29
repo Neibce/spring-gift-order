98e2b477ad1e723b7111ef43868abc5bf998509d
Junyoung Yang <y@jun0.dev>
2025-07-30 07:57:38 +0900

    feat: OrderService 구현 완료 (주문하기 기능(수량 차감, 위시 리스트 삭제)+카카오 메시지 발송 기능)

---
+++ src/main/java/gift/oauth/kakao/dto/KakaoTokenRenewDto.java
@@ -0,0 +1,27 @@
+package gift.oauth.kakao.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record KakaoTokenRenewDto(
+        @JsonProperty("token_type")
+        String tokenType,
+
+        @JsonProperty("access_token")
+        String accessToken,
+
+        @JsonProperty("id_token")
+        String idToken,
+
+        @JsonProperty("expires_in")
+        Integer expiresIn,
+
+        @JsonProperty("refresh_token")
+        String refreshToken,
+
+        @JsonProperty("refresh_token_expires_in")
+        Integer refreshTokenExpiresIn
+) {
+
+}
